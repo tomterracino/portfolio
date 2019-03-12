9179f3b2ca6f8d51fae30027eb7a157daefa147d
Tom Terracino <terracino1@yahoo.com>
2019-03-11 20:08:40 -0400

    Updates

---
+++ precache-manifest.32c74ced8ca5813481dc16a9d6af07b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "d23e86017d7a41d7800d",
+    "url": "/portfolio/static/js/main.d23e8601.chunk.js"
+  },
+  {
+    "revision": "15c8bd49e1aceaa97acf",
+    "url": "/portfolio/static/js/2.15c8bd49.chunk.js"
+  },
+  {
+    "revision": "d23e86017d7a41d7800d",
+    "url": "/portfolio/static/css/main.28eb5c3e.chunk.css"
+  },
+  {
+    "revision": "15c8bd49e1aceaa97acf",
+    "url": "/portfolio/static/css/2.5bf47e6c.chunk.css"
+  },
+  {
+    "revision": "8255084e20c4482711f47ecb215072a0",
+    "url": "/portfolio/index.html"
+  }
+];
