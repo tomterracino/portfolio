7f485d944689b908ede1b508c2563c0f50445c58
Tom Terracino <terracino1@yahoo.com>
2019-03-11 23:42:51 -0400

    Updates

--- precache-manifest.4d01befce184d8bb6e9507659e4d51c0.js
+++ precache-manifest.4d01befce184d8bb6e9507659e4d51c0.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
   },
   {
-    "revision": "f8329cf33f705c26d330",
-    "url": "/portfolio/static/js/main.f8329cf3.chunk.js"
+    "revision": "ab70b7e212f440591643",
+    "url": "/portfolio/static/js/main.ab70b7e2.chunk.js"
   },
   {
     "revision": "566809461ccb3caebe16",
     "url": "/portfolio/static/js/2.56680946.chunk.js"
   },
   {
-    "revision": "f8329cf33f705c26d330",
-    "url": "/portfolio/static/css/main.9881e8cc.chunk.css"
+    "revision": "ab70b7e212f440591643",
+    "url": "/portfolio/static/css/main.0d0a4309.chunk.css"
   },
   {
     "revision": "566809461ccb3caebe16",
     "url": "/portfolio/static/css/2.5bf47e6c.chunk.css"
   },
   {
-    "revision": "c6a8d458972bb406ddcaac49b34ade86",
+    "revision": "90021fa77280de2454626d8195b468a5",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9f6859baa9bd0ba3f091844d76fa6c33.js"
+  "/portfolio/precache-manifest.4d01befce184d8bb6e9507659e4d51c0.js"
 );
 
 workbox.clientsClaim();
